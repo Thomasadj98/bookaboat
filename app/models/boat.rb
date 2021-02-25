b360dee138970b388ca18664c1fc8109679ae530
Thomas de Jong <thomasalexanderdejong@gmail.com>
2021-02-25 14:39:52 +0100

    cloudinary added to boat and user

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,6 @@
 class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+pet_picture = URI.open('https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png')
+user1 = User.new(username: "Snoopy", email: "thomas@gmail.com", password: "password123")
+user1.photo.attach(io: pet_picture, filename: 'pet1.png', content_type: 'image/png')
+user1.save!
